35bd2ae251ef80f2a6b911f550132e61b008961e
Artyom Pishalnikov <82143842+lemonmcline@users.noreply.github.com>
2021-04-23 22:42:03 +0600

    Odnomer-masiv
    Odnomer-masiv

---
+++ Odnomer masiv.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+
+    int a[] = { 66,32,27,89,14 };
+
+    cout << a[3] << " ";
+    
+    for (int i = 0; i < n;i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
+    int b[10];
+
+    for (int i = 0; i < 10; i++) {
+        b[i] = i;
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << b[i] << " ";
+    }
+
+    cout << endl;
+
+    int c[10];
+    int lfg=5 ;
+    for (int i = 0; i < 10; i++) {
+        c[i] = i;
+        lfg = lfg + c[i];
+        cout << lfg << " ";
+    }
+
+    cout << endl;
+    cout << "lfg " << lfg;
+}
